b34ea0f7a0775e02132c846220efd81cba493ff7
JonathanDiz <you@example.com>
2023-01-12 09:13:37 -0300

    guardado de codigo 18

--- todolist.js
+++ todolist.js
@@ -8,7 +8,7 @@ const client = new Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD.toString(),
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
